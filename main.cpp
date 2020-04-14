c7fcc839e4067d61ef69ca32bde14e05e7d5fe73
Sazona <twonapom@gmail.com>
2020-04-14 16:31:29 +0800

    添加 TcpServer

--- main.cpp
+++ main.cpp
@@ -1,35 +1,55 @@
 #include "muduo/base/Thread.h"
 #include "muduo/base/Logging.h"
+#include "muduo/net/TcpServer.h"
+#include "muduo/net/EventLoop.h"
 
 #include <iostream>
 #include <stdio.h>
 
+using namespace std;
 using namespace muduo;
+using namespace muduo::net;
 
-void threadFunc() 
+void newconn(const TcpConnectionPtr& conn)
 {
-	LOG_TRACE << "thread TRACE";
-	LOG_DEBUG << "thread DEBUG";
-	LOG_INFO << "thread  INFO";
-	LOG_WARN << "thread  WARN";
-	LOG_ERROR << "thread ERROR";
-	LOG_SYSERR << "thread SYSERR";
-
+	cout << "newconn: " << conn->getTcpInfoString();
 }
 
-void test_Log()
+void newmessage(const TcpConnectionPtr& conn, Buffer* buf, Timestamp t)
 {
-	std::cout << "----- Log -----\n";
-	
-	Thread t1(threadFunc);
+	cout << "newmessage: " << "readableBytes = " << buf->readableBytes() << ", " << buf->retrieveAllAsString();
+	conn->send(Timestamp::now().toString() + '\n');
+}
 
-	t1.start();
-	t1.join();
+void threadInit(EventLoop*) 
+{
+	cout << "Just empty threadInit";
+}
+
+void writeComplete(const TcpConnectionPtr& conn)
+{
+	cout << conn->name() << " " << conn->peerAddress().toIpPort() << " writeComplete";
+}
+
+void cb()
+{
+	cout << "cb() is calling!";
 }
 
 int main() {
 
-	test_Log();
+	Logger::setLogLevel(Logger::DEBUG);
+	InetAddress listenAddr("0.0.0.0", 9999);
+	EventLoop loop;
+	TcpServer server(&loop, listenAddr, "TcpServer");
+	server.setConnectionCallback(newconn);
+	server.setMessageCallback(newmessage);
+	server.setThreadNum(3);
+	server.setThreadInitCallback(threadInit);
+	server.setWriteCompleteCallback(writeComplete);
+	server.start();
+
+	loop.loop();
 
 	return 0;
 }
---
+++ muduo/net/TcpServer.h
@@ -0,0 +1,84 @@
+#ifndef MUDUO_NET_TCPSERVER_H
+#define MUDUO_NET_TCPSERVER_H
+
+#include "muduo/base/Atomic.h"
+#include "muduo/base/Types.h"
+#include "muduo/net/TcpConnection.h"
+
+#include <map>
+
+namespace muduo
+{
+namespace net
+{
+
+class Acceptor;
+class EventLoop;
+class EventLoopThreadPool;
+
+// Tcp server, supports single threaded and thread-pool models.
+class TcpServer : noncopyable
+{
+public:
+	typedef std::function<void (EventLoop*)> ThreadInitCallback;
+	enum Option
+	{
+		kNoReusePort,
+		kReusePort,
+	};
+
+	TcpServer(EventLoop* loop,
+			  const InetAddress& listenAddr,
+			  const string& nameArg,
+			  Option option = kNoReusePort);
+	~TcpServer();
+	const string& ipPort() const { return ipPort(); }
+	const string& name() const { return name_; }
+	EventLoop* getLoop() const { return loop_; }
+	/// Set the number of threads for handling input.
+	///
+	/// Always accepts new connection in loop's thread.
+	/// Must be called before @c start
+	/// @param numThreads
+	/// - 0 means all I/O in loop's thread, no thread will created.
+	///   this is the default value.
+	/// - 1 means all I/O in another thread.
+	/// - N means a thread pool with N threads, new connections
+	///   are assigned on a round-robin basis.	
+	void setThreadNum(int numThreads);
+	void setThreadInitCallback(const ThreadInitCallback& cb) { threadInitCallback_ = cb; }
+	std::shared_ptr<EventLoopThreadPool> threadPool() { return threadPool_; }
+	
+	void start();
+	void setConnectionCallback(const ConnectionCallback& cb) { connectionCallback_ = cb; }
+	void setMessageCallback(const MessageCallback& cb) { messageCallback_ = cb; }
+	void setWriteCompleteCallback(const WriteCompleteCallback& cb) { writeCompleteCallback_ = cb; }
+
+private:
+
+	void newConnection(int sockfd, const InetAddress& peerAddr);
+	void removeConnection(const TcpConnectionPtr& conn);
+	void removeConnectionInLoop(const TcpConnectionPtr& conn);
+	typedef std::map<string, TcpConnectionPtr> ConnectionMap;
+	
+	EventLoop* loop_; // acceptor loop
+	const string ipPort_;
+	const string name_;
+	std::unique_ptr<Acceptor> acceptor_;
+	std::shared_ptr<EventLoopThreadPool> threadPool_;
+	ConnectionCallback connectionCallback_;
+	MessageCallback messageCallback_;
+	WriteCompleteCallback writeCompleteCallback_;
+	ThreadInitCallback threadInitCallback_;
+	AtomicInt32 started_;
+	
+	int nextConnId_;
+	ConnectionMap connections_;
+}; // class TcpServer
+
+} // namespace net
+
+
+} // namespace muduo
+
+#endif // MUDUO_NET_TCPSERVER_H
