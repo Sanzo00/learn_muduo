12eb5b84dc32fea696416a770f2039cb4e9ec31d
Sanzona <twonapom@gmail.com>
2020-05-08 13:33:16 +0100

    添加 HttpServer

---
+++ muduo/net/http/HttpResponse.cpp
@@ -0,0 +1,33 @@
+#include "muduo/net/http/HttpResponse.h"
+#include "muduo/net/Buffer.h"
+
+#include <stdio.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+void HttpResponse::appendToBuffer(Buffer* output) const
+{
+	char buf[32];
+	snprintf(buf, sizeof buf, "HTTP/1.1 %d ", statusCode_);
+	output->append(buf);
+	output->append(statusMessage_);
+	output->append("\r\n");
+
+	if (closeConnection_) {
+		output->append("Connection: close\r\n");
+	}else {
+		snprintf(buf, sizeof buf, "Content-Length: %zd\r\n", body_.size()); // size_t
+		output->append(buf);
+		output->append("Connection: Keep-ALive\r\n");
+	}
+
+	for (const auto& header : headers_) {
+		output->append(header.first);
+		output->append(": ");
+		output->append(header.second);
+		output->append("\r\n");
+	}
+	output->append("\r\n");
+	output->append(body_);
+}
